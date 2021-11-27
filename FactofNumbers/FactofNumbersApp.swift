1ef72d365d4ef3e050f0d98b3301c2e16e858dea
Xun Ruan <galaxymacos@gmail.com>
2021-11-27 22:40:50 +0800

    Initial Commit

---
+++ FactofNumbers/FactofNumbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  FactofNumbersApp.swift
+//  FactofNumbers
+//
+//  Created by xunruan on 2021-11-27.
+//
+
+import SwiftUI
+
+@main
+struct FactofNumbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
